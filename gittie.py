243aab8563b9a18d40fbdf93ab6dfa74a82ca1c2
Adam Borbely <borbelya90@gmail.com>
2019-10-17 10:11:21 +0200

    Hmm

--- gittie.py
+++ gittie.py
@@ -9,11 +9,8 @@ def joke():
 def add():
     print("add")
 
-<<<<<<< HEAD
 
 def Géza():
     print("Géza")
-=======
 def night_at_the_hood():
     print("where is th hood at?")
->>>>>>> 185cb0a3219ca953b5721633b6ce601722f70540
