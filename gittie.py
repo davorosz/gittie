2a91d1dd7b43e0884a3b9345b069e8770d865e0f
Adam Borbely <borbelya90@gmail.com>
2019-10-17 09:51:01 +0200

    best joke eva

--- gittie.py
+++ gittie.py
@@ -1,2 +1,5 @@
 def introduce():
     print("My name is gittie, I have a big titty")
+
+def joke():
+    print("Today, my son asked \"Can I have a book mark?\" and I burst into tears. 11 years old and he still doesn't know my name is Brian.")
