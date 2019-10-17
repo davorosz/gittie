f0faf890e9d9e1e8fd3cc5555f370bf734542536
David Orosz <davorosz254@gmail.com>
2019-10-17 10:02:45 +0200

    efaf

--- gittie.py
+++ gittie.py
@@ -1,8 +1,14 @@
 def introduce():
     print("My name is gittie, I have a big titty")
 
+
 def joke():
     print("Today, my son asked \"Can I have a book mark?\" and I burst into tears. 11 years old and he still doesn't know my name is Brian.")
 
+
 def add():
     print("add")
+
+
+def Géza():
+    print("Géza")
